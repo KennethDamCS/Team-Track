14e0e5077bcb52a4924cd56fe578c8af67a02679
Kenneth Dam <kennethdam@Kenneths-MacBook-Pro.local>
2024-01-08 17:23:25 -0800

    Spring project file creation

---
+++ teamtrack-server/teamtrackserver/src/test/java/kennethdamcs/teamtrackserver/TeamtrackserverApplicationTests.java
@@ -0,0 +1,13 @@
+package kennethdamcs.teamtrackserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamtrackserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
