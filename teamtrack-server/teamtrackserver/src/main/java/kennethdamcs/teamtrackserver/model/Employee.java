28ad734e398ed26f906f49f0da180279c2a50b68
Kenneth Dam <kennethdam@Kenneths-MacBook-Pro.local>
2024-01-09 21:31:17 -0800

    Employee - controller w/ get all rest api, entity, repository, and exception

---
+++ teamtrack-server/teamtrackserver/src/main/java/kennethdamcs/teamtrackserver/model/Employee.java
@@ -0,0 +1,55 @@
+package kennethdamcs.teamtrackserver.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email_id")
+    private String emailId;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+}
---
+++ teamtrack-server/teamtrackserver/src/main/java/kennethdamcs/teamtrackserver/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package kennethdamcs.teamtrackserver.repository;
+
+import kennethdamcs.teamtrackserver.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
